2fec401da95ad90575147103a6299621585676de
Nguyễn Hoàng Nam <nguyenhoangnam.dev@gmail.com>
2020-07-28 14:03:52 +0700

    ✨ Check input file
    _ Check MIME type:
     |_ Check file.type:
     | |_ image/jpeg
     |  _ image/png
     |  _ image/webp
     |  _ image/bmp
     |_ Check first 4 bytes:
     | |_ jpg: ffd8ff
     |  _ png: 89504e47
     |  _ webp: 57454250
     |  _ bmp: 424d

--- js/index.js
+++ js/index.js
@@ -1,5 +1,38 @@
 let type = document.getElementById("image-type");
 let quality = document.getElementById("image-quality");
+let imagePattern = new RegExp("image/(png|jpeg|webp|bmp)");
+
+let MIME = {
+  "image/jpeg": {
+    ext: "jpg",
+    pattern: [0xff, 0xd8, 0xff],
+    mask: [0xff, 0xff, 0xff],
+  },
+  "image/png": {
+    ext: "png",
+    pattern: [0x89, 0x50, 0x4e, 0x47],
+    mask: [0xff, 0xff, 0xff, 0xff],
+  },
+  "image/webp": {
+    ext: "webp",
+    pattern: [0x57, 0x45, 0x42, 0x50],
+    mask: [0xff, 0xff, 0xff, 0xff],
+  },
+  "image/bmp": {
+    ext: "bmp",
+    pattern: [0x42, 0x4d],
+    mask: [0xff, 0xff],
+  },
+};
+
+function check(bytes, mime) {
+  for (var i = 0, l = mime.mask.length; i < l; ++i) {
+    if ((bytes[i] & mime.mask[i]) - mime.pattern[i] !== 0) {
+      return false;
+    }
+  }
+  return true;
+}
 
 function roundBytes(bytes) {
   if (bytes >= 1048576) return Math.round((bytes / 1048576) * 10) / 10 + "MB";
@@ -12,116 +45,137 @@ function disableZoomIn() {
   $(".zoom-selector").remove();
 }
 
-function handleFiles() {
-  $("#panel-upload").addClass("disable");
-
+function checkMIME() {
   const fileList = this.files;
   let image = fileList[0];
 
   let inputSize = image.size;
   let inputName = image.name;
+  let inputType = image.type;
+  let fileBlob = image.slice(0, 4);
 
   $("#input-name").text(inputName);
   $("#input-size").text(roundBytes(inputSize));
 
-  let imagePattern = new RegExp("image/(png|jpeg|webp|bmp)");
-
-  if (!imagePattern.test(image.type)) {
-    console.log("This file is not an image");
+  // Check mime type
+  if (!imagePattern.test(inputType)) {
+    alert("Invalid image type");
   } else {
-    let imageBlob = URL.createObjectURL(image);
-    $(".upload-image").attr("src", imageBlob);
+    let reader = new FileReader();
+    reader.readAsArrayBuffer(fileBlob);
 
-    $(".beforeafterdefault").cndkbeforeafter({
-      mode: "drag",
-      beforeTextPosition: "top-left",
-      afterTextPosition: "top-right",
-    });
-
-    // Create temporary image to get width, height, load event
-    let img = document.createElement("img");
-    img.src = imageBlob;
-    img.style.width = "auto";
-    img.style.height = "auto";
-
-    img.onerror = function () {
-      URL.revokeObjectURL(this.src);
-      console.log("Can not load image");
-    };
-
-    img.onload = function (e) {
-      let startType = image.type.split("/")[1];
-      if (startType == "jpeg") startType = "jpg";
-
-      let imgWidth = e.target.width;
-      let imgHeight = e.target.height;
-      let scale = imgWidth / imgHeight;
-
-      $("#width").val(imgWidth);
-      $("#height").val(imgHeight);
-
-      let canvas = document.createElement("canvas");
-      canvas.width = imgWidth;
-      canvas.height = imgHeight;
-      let ctx = canvas.getContext("2d");
-      ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
-
-      $("#convert").addClass("ready-convert");
-
-      $("#width").on("input", function () {
-        let newImgWidth = $(this).val();
-        let newImgHeight = newImgWidth / scale;
-        $("#height").val(newImgHeight);
-
-        canvas.width = newImgWidth;
-        canvas.height = newImgHeight;
-        ctx.drawImage(img, 0, 0, newImgWidth, newImgHeight);
-      });
-
-      $("#height").on("input", function () {
-        let newImgHeight = $(this).val();
-        let newImgWidth = newImgHeight * scale;
-        $("#width").val(newImgWidth);
-
-        canvas.width = newImgWidth;
-        canvas.height = newImgHeight;
-        ctx.drawImage(img, 0, 0, newImgWidth, newImgHeight);
-      });
-
-      $("#convert").on("click", function () {
-        let destType = type.value;
-        let ratio = parseInt(quality.value) / 10;
-
-        let newFileName = image.name.replace(startType, destType);
-
-        if (destType == "jpg") destType = "jpeg";
-
-        canvas.toBlob(
-          function (blob) {
-            let outputSize = roundBytes(blob.size);
-            $("#output-name").text(newFileName);
-            $("#output-size").text(outputSize);
-
-            let newImageBlob = URL.createObjectURL(blob);
-            $(".download-image").attr("src", newImageBlob);
-            $(".btn-download").removeClass("disable");
-
-            $("#download").attr("download", newFileName);
-            $("#download").attr("href", newImageBlob);
-          },
-          "image/" + destType,
-          ratio
-        );
-      });
+    reader.onloadend = function (e) {
+      if (e.target.readyState === FileReader.DONE) {
+        let bytes = new Uint8Array(e.target.result);
+        if (check(bytes, MIME[inputType])) {
+          if (inputSize > 52428800) {
+            alert("File should be <= 50MB");
+          } else {
+            handleFiles(image);
+          }
+        } else {
+          alert("Invalid image type");
+        }
+      }
     };
   }
+}
+
+function handleFiles(image) {
+  $("#panel-upload").addClass("disable");
+
+  let imageBlob = URL.createObjectURL(image);
+  $(".upload-image").attr("src", imageBlob);
+
+  $(".beforeafterdefault").cndkbeforeafter({
+    mode: "drag",
+    beforeTextPosition: "top-left",
+    afterTextPosition: "top-right",
+  });
+
+  // Create temporary image to get width, height, load event
+  let img = document.createElement("img");
+  img.src = imageBlob;
+  img.style.width = "auto";
+  img.style.height = "auto";
+
+  img.onerror = function () {
+    URL.revokeObjectURL(this.src);
+    console.log("Can not load image");
+  };
+
+  img.onload = function (e) {
+    let startType = image.type.split("/")[1];
+    if (startType == "jpeg") startType = "jpg";
+
+    let imgWidth = e.target.width;
+    let imgHeight = e.target.height;
+    let scale = imgWidth / imgHeight;
+
+    $("#width").val(imgWidth);
+    $("#height").val(imgHeight);
+
+    let canvas = document.createElement("canvas");
+    canvas.width = imgWidth;
+    canvas.height = imgHeight;
+    let ctx = canvas.getContext("2d");
+    ctx.drawImage(img, 0, 0, imgWidth, imgHeight);
+
+    $("#convert").addClass("ready-convert");
+
+    $("#width").on("input", function () {
+      let newImgWidth = $(this).val();
+      let newImgHeight = newImgWidth / scale;
+      $("#height").val(newImgHeight);
+
+      canvas.width = newImgWidth;
+      canvas.height = newImgHeight;
+      ctx.drawImage(img, 0, 0, newImgWidth, newImgHeight);
+    });
+
+    $("#height").on("input", function () {
+      let newImgHeight = $(this).val();
+      let newImgWidth = newImgHeight * scale;
+      $("#width").val(newImgWidth);
+
+      canvas.width = newImgWidth;
+      canvas.height = newImgHeight;
+      ctx.drawImage(img, 0, 0, newImgWidth, newImgHeight);
+    });
+
+    $("#convert").on("click", function () {
+      let destType = type.value;
+      let ratio = parseInt(quality.value) / 10;
+
+      let newFileName = image.name.replace(startType, destType);
+
+      if (destType == "jpg") destType = "jpeg";
+
+      canvas.toBlob(
+        function (blob) {
+          let outputSize = roundBytes(blob.size);
+          $("#output-name").text(newFileName);
+          $("#output-size").text(outputSize);
+
+          let newImageBlob = URL.createObjectURL(blob);
+          $(".download-image").attr("src", newImageBlob);
+          $(".btn-download").removeClass("disable");
+
+          $("#download").attr("download", newFileName);
+          $("#download").attr("href", newImageBlob);
+        },
+        "image/" + destType,
+        ratio
+      );
+    });
+  };
 }
 
 $("#btn-upload").on("click", function () {
   $("#upload").click();
 });
 
-$("#upload").on("change", handleFiles);
+$("#upload").on("change", checkMIME);
 
 $(".btn-download").on("click", function () {
   $("#download")[0].click();
